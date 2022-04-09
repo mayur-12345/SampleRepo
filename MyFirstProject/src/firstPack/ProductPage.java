1da4781579b2502458c393a56bcca89726006aae
mayur-12345 <103274805+mayur-12345@users.noreply.github.com>
2022-04-09 12:55:52 +0530

    newMethodAddedInProductPage
    newMethodAddedInProductPage.Please review it.

--- MyFirstProject/src/firstPack/ProductPage.java
+++ MyFirstProject/src/firstPack/ProductPage.java
@@ -1,9 +1,15 @@
 package firstPack;
 
 public class ProductPage {
-
+	public static void newMethod() {
+		System.out.println("New method");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Product page");
+		newMethod();
 	}
+	
+	
 
 }
